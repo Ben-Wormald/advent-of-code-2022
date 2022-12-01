ced429c5fbb75d0d0b72ce919ab508588fbbf790
Ben-Wormald <benwormald95@gmail.com>
2022-12-01 09:50:02 +0000

    solve day 1

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![allow(dead_code)]
+use std::fs;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(short, long)]
+    test: bool,
+}
+
+mod solutions { pub mod day_01; }
+use solutions::day_01::solve;
+const INPUT: &str = "./input/01";
+const TEST_INPUT: &str = "./input/test";
+
+fn main() {
+    let opt = Opt::from_args();
+    let input = if opt.test { TEST_INPUT } else { INPUT };
+    let input = fs::read_to_string(input).expect("oh no!");
+
+    let result = solve(&input);
+    println!("{}", result)
+}
---
+++ src/solutions/day_01.rs
@@ -0,0 +1,34 @@
+fn process(input: &str) -> Vec<usize> {
+    input
+        .split("\n\n")
+        .map(|elf| elf
+            .split_whitespace()
+            .map(|cals| cals
+                .parse::<usize>()
+                .unwrap()
+            )
+            .sum()
+        )
+        .collect()
+}
+
+pub fn solve_part_one(input: &str) -> usize {
+    let elves = process(input);
+
+    elves
+        .into_iter()
+        .reduce(|max, elf| std::cmp::max(max, elf))
+        .unwrap()
+}
+
+pub fn solve(input: &str) -> usize {
+    let mut elves = process(input);
+
+    elves.sort_by(|a, b| b.cmp(a));
+    
+    elves
+        .into_iter()
+        .take(3)
+        .reduce(|sum, elf| sum + elf)
+        .unwrap()
+}
