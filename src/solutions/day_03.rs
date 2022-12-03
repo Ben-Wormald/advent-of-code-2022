39a6c6e8493cd0d612788fd4fde217a3339b0d6c
Ben Wormald <benwormald95@gmail.com>
2022-12-03 13:37:04 +0000

    solve day 3 pt 2

--- src/solutions/day_03.rs
+++ src/solutions/day_03.rs
@@ -1,4 +1,4 @@
-type Rucksack<'a> = (&'a str, &'a str);
+use itertools::Itertools;
 
 trait ToValue {
     fn to_value(&self) -> u32;
@@ -14,26 +14,38 @@ impl ToValue for char {
     }
 }
 
-fn process(input: &str) -> Vec<Rucksack> {
+fn process(input: &str) -> Vec<(&str, &str)> {
     input
         .lines()
-        .map(|rucksack| rucksack
-            .split_at(rucksack.len() / 2)
+        .map(|rucksack|
+            rucksack.split_at(rucksack.len() / 2)
         )
         .collect()
 }
 
-pub fn solve(input: &str) -> u32 {
-    let rucksacks = process(input);
-
-    rucksacks
+pub fn solve_part_one(input: &str) -> u32 {
+    process(input)
         .into_iter()
-        .fold(0, |sum, rucksack| {
-            let (compartment_a, compartment_b) = rucksack;
+        .fold(0, |sum, (compartment_a, compartment_b)| {
             sum + compartment_a
                 .chars()
-                .find(|item| compartment_b
-                    .contains(*item)
+                .find(|item|
+                    compartment_b.contains(*item)
+                )
+                .unwrap()
+                .to_value()
+        })
+}
+
+pub fn solve(input: &str) -> u32 {
+    input
+        .lines()
+        .tuples()
+        .fold(0, |sum, (backpack_a, backpack_b, backpack_c)| {
+            sum + backpack_a
+                .chars()
+                .find(|item|
+                    backpack_b.contains(*item) && backpack_c.contains(*item)
                 )
                 .unwrap()
                 .to_value()
